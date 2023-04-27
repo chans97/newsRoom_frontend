43915dc7add8984812c2668b2f4826ad3827fab0
bentley <papajuju123@naver.com>
2023-04-27 13:55:49 +0900

    feature: PWA 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,26 @@ import type { AppProps } from 'next/app'
 import { LoginWrapper } from '../context/loginstate';
 import '../public/reset.css'
 import '../public/font.css'
+import { useEffect } from 'react';
 
 function App({ Component, pageProps }: AppProps) {
-
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      window.addEventListener("load", function () {
+        navigator.serviceWorker.register("/sw.js").then(
+          function (registration) {
+            console.log(
+              "Service Worker registration successful with scope: ",
+              registration.scope,
+            );
+          },
+          function (err) {
+            console.log("Service Worker registration failed: ", err);
+          },
+        );
+      });
+    }
+  }, []);
 
   return (
     <LoginWrapper>
---
+++ pages/_document.tsx
@@ -0,0 +1,71 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+    return (
+        <Html lang="en">
+            <Head >
+                <meta name="theme-color" content="#FF98BA"></meta>
+                <link rel="manifest" href="/manifest.json" />
+                <link
+                    href="/public/favicons/favicon-16x16.png"
+                    rel="icon"
+                    type="image/png"
+                    sizes="16x16"
+                />
+                <link
+                    href="/public/favicons/favicon-32x32.png"
+                    rel="icon"
+                    type="image/png"
+                    sizes="32x32"
+                />
+                <link rel="apple-touch-icon" href="/public/favicons/apple-icon-192x192.png"></link>
+                <meta name="msapplication-TileColor" content="#FF98BA"></meta>
+                <link
+                    rel="apple-touch-startup-image"
+                    href="/public/splash/splash.png"
+                    media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+                ></link>
+                <link
+                    rel="apple-touch-startup-image"
+
+                    href="/public/splash/splash.png"
+                    media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+                ></link>
+                <link
+                    rel="apple-touch-startup-image"
+
+                    href="/public/splash/splash.png"
+                    media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
+                ></link>
+                <link
+                    rel="apple-touch-startup-image"
+
+                    href="/public/splash/splash.png"
+                    media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
+                ></link>
+                <link
+                    rel="apple-touch-startup-image"
+
+                    href="/public/splash/splash.png"
+                    media="(min-device-width: 768px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)"
+                ></link>
+                <link
+                    rel="apple-touch-startup-image"
+
+                    href="/public/splash/splash.png"
+                    media="(min-device-width: 834px) and (max-device-width: 834px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)"
+                ></link>
+                <link
+                    rel="apple-touch-startup-image"
+
+                    href="/public/splash/splash.png"
+                    media="(min-device-width: 1024px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)"
+                ></link>
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
+}
